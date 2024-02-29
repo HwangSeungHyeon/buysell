57b4bd8cf6c51c92964246be926f7ddb07cfa06e
lazzzykim <33sunghyeon@gmail.com>
2024-03-01 03:00:38 +0900

    ✨Feat: Review Update 구현
    ReviewController: Update 요청 메서드 작성
    Review: post fetchType.EAGER -> LAZY 변경
    ReviewServiceImpl: 리뷰 수정 비즈니스 로직 작성
    UpdateReviewRequest: UpdateRequest 작성
    
    resolves: #6

--- src/main/kotlin/com/teamsparta/buysell/domain/review/service/ReviewServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/review/service/ReviewServiceImpl.kt
@@ -4,10 +4,12 @@ import com.teamsparta.buysell.domain.exception.ModelNotFoundException
 import com.teamsparta.buysell.domain.member.repository.MemberRepository
 import com.teamsparta.buysell.domain.post.repository.PostRepository
 import com.teamsparta.buysell.domain.review.dto.request.CreateReviewRequest
+import com.teamsparta.buysell.domain.review.dto.request.UpdateReviewRequest
 import com.teamsparta.buysell.domain.review.dto.response.ReviewResponse
 import com.teamsparta.buysell.domain.review.model.Review
 import com.teamsparta.buysell.domain.review.repository.ReviewRepository
 import com.teamsparta.buysell.infra.security.UserPrincipal
+import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
@@ -16,10 +18,11 @@ class ReviewServiceImpl(
     private val reviewRepository: ReviewRepository,
     private val postRepository: PostRepository,
     private val memberRepository: MemberRepository
-): ReviewService {
-    override fun createReview(postId: Int,
-                              request: CreateReviewRequest,
-                              principal: UserPrincipal
+) : ReviewService {
+    override fun createReview(
+        postId: Int,
+        request: CreateReviewRequest,
+        principal: UserPrincipal
     ): ReviewResponse {
         val post = postRepository.findByIdOrNull(postId)
             ?: throw ModelNotFoundException("Post", postId)
@@ -32,5 +35,21 @@ class ReviewServiceImpl(
             member = member
         ).let { reviewRepository.save(it) }
         return ReviewResponse("리뷰가 작성되었습니다.")
+    }
+
+    @Transactional
+    override fun editReview(
+        reviewId: Int,
+        postId: Int,
+        request: UpdateReviewRequest,
+        principal: UserPrincipal
+    ): ReviewResponse {
+        val review = reviewRepository.findByIdOrNull(reviewId)
+            ?: throw ModelNotFoundException("Review", reviewId)
+
+        review.checkPermission(principal)
+        review.edit(request)
+
+        return ReviewResponse("리뷰가 수정되었습니다.")
     }
 }
