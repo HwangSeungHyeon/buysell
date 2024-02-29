f7cb544e8d62296b7780bf348236bff3e1f9fb71
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-01 01:29:57 +0900

    ✨Feat: 게시글 Pagination
    게시글을 조회할 때 QueryDSL을 사용한 pagination 을 구현
    
    Resolves: #1

--- src/main/kotlin/com/teamsparta/buysell/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/post/controller/PostController.kt
@@ -2,9 +2,16 @@ package com.teamsparta.buysell.domain.post.controller
 
 import com.teamsparta.buysell.domain.post.dto.request.CreatePostRequest
 import com.teamsparta.buysell.domain.post.dto.request.UpdatePostRequest
+import com.teamsparta.buysell.domain.post.dto.response.PostListResponse
 import com.teamsparta.buysell.domain.post.dto.response.PostResponse
 import com.teamsparta.buysell.domain.post.service.PostService
 import com.teamsparta.buysell.infra.security.UserPrincipal
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -40,10 +47,20 @@ class PostController(
     }
 
     @GetMapping
-    fun getPosts(): ResponseEntity<List<PostResponse>> {
+    fun getPosts(
+        @Valid @NotBlank
+        @Size(min = 1, max = 15, message = "1자 이상 15자 이하여야 합니다.")
+        @RequestParam keyword: String,
+
+        @PageableDefault(
+            page = 0,
+            size = 10,
+            sort = ["title"]
+        ) pageable: Pageable
+    ): ResponseEntity<Page<PostListResponse>> {
         return ResponseEntity
             .status(HttpStatus.OK)
-            .body(postService.getPosts())
+            .body(postService.searchByKeyword(keyword, pageable))
     }
 
     @GetMapping("/{postId}")
--- src/main/kotlin/com/teamsparta/buysell/domain/post/repository/PostRepository.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/post/repository/PostRepository.kt
@@ -3,6 +3,6 @@ package com.teamsparta.buysell.domain.post.repository
 import com.teamsparta.buysell.domain.post.model.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository: JpaRepository<Post, Int> {
+interface PostRepository: JpaRepository<Post, Int>, CustomPostRepository {
 
 }
