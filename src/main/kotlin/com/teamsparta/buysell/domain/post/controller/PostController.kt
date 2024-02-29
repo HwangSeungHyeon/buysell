5bf3bc8e88c5690acc22017efc6e8b28b1279e82
lazzzykim <33sunghyeon@gmail.com>
2024-02-29 18:10:33 +0900

    ✨Feat: 게시물 CRUD 구현
    PostController: CRUD 요청 메서드 작성
    PostResponse: post response 작성
    CreatePostRequest: 게시물 create request 코드 작성
    UpdatePostRequest: 게시물 update request 코드 작성
    Post: post 관련 Entity 작성
    PostRepository: PostRepository 생성
    PostServiceImpl: 게시물 CRUD 비즈니스 로직 작성

--- src/main/kotlin/com/teamsparta/buysell/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/post/controller/PostController.kt
@@ -1,4 +1,61 @@
 package com.teamsparta.buysell.domain.post.controller
 
-class PostController {
+import com.teamsparta.buysell.domain.post.dto.request.CreatePostRequest
+import com.teamsparta.buysell.domain.post.dto.request.UpdatePostRequest
+import com.teamsparta.buysell.domain.post.dto.response.PostResponse
+import com.teamsparta.buysell.domain.post.service.PostService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RequestMapping("/posts")
+@RestController
+class PostController(
+    private val postService: PostService
+) {
+
+    @PostMapping
+    fun createPost(
+        @RequestBody createPostRequest: CreatePostRequest
+    ): ResponseEntity<PostResponse> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(postService.createPost(createPostRequest))
+    }
+
+    @PutMapping("/{postId}")
+    fun updatePost(
+        @PathVariable postId: Int,
+        @RequestBody updateRequest: UpdatePostRequest
+    ): ResponseEntity<PostResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(postService.updatePost(postId, updateRequest))
+    }
+
+    @GetMapping
+    fun getPosts(): ResponseEntity<List<PostResponse>> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(postService.getPosts())
+    }
+
+    @GetMapping("/{postId}")
+    fun getPostbyId(
+        @PathVariable postId: Int
+    ): ResponseEntity<PostResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(postService.getPostById(postId))
+    }
+
+    @DeleteMapping("/{postId}")
+    fun deletePost(
+        @PathVariable postId: Int
+    ): ResponseEntity<Unit> {
+        postService.deletePost(postId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
+    }
 }
---
+++ src/main/kotlin/com/teamsparta/buysell/domain/post/dto/request/CreatePostRequest.kt
@@ -0,0 +1,10 @@
+package com.teamsparta.buysell.domain.post.dto.request
+
+data class CreatePostRequest(
+    val title: String,
+    val content: String,
+    val price: Int,
+//    val category: Category
+//    val route:
+//    val imgUrl:
+)
