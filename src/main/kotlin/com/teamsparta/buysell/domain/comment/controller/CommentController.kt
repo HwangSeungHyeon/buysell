3d9877eecb75b34a060b3bcbe590d87fb557c512
HwangSeungHyeon <xlddy02@gmail.com>
2024-02-29 18:13:09 +0900

    ✨Feat: 댓글 CRUD 구현
    댓글 작성
    
    Resolves: #11

--- src/main/kotlin/com/teamsparta/buysell/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/comment/controller/CommentController.kt
@@ -1,4 +1,35 @@
 package com.teamsparta.buysell.domain.comment.controller
 
-class CommentController {
+import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
+import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
+import com.teamsparta.buysell.domain.comment.dto.response.CommentResponse
+import com.teamsparta.buysell.domain.comment.service.CommentService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RequestMapping("/posts/{postId}/comments")
+@RestController
+class CommentController(
+    private val commentService: CommentService
+) {
+    @PostMapping
+    fun addComment(
+        @PathVariable postId: String,
+        @RequestBody request: CreateRequest
+    ) : ResponseEntity<CommentResponse>{
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(commentService.addService(postId, request))
+    }
+
+    @PutMapping
+    fun editComment(
+        @PathVariable postId: String,
+        @RequestBody request: UpdateRequest
+    ) : ResponseEntity<CommentResponse>{
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(commentService.editComment(postId, request))
+    }
 }
--- src/main/kotlin/com/teamsparta/buysell/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/comment/service/CommentService.kt
@@ -1,4 +1,11 @@
 package com.teamsparta.buysell.domain.comment.service
 
+import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
+import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
+import com.teamsparta.buysell.domain.comment.dto.response.CommentResponse
+
 interface CommentService {
+    fun addService(postId: Int, request: CreateRequest): CommentResponse
+
+    fun editService(postId: Int, request: UpdateRequest): CommentResponse
 }
