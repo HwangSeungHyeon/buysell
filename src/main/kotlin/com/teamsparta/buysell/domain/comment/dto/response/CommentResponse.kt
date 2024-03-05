6baea7308dfaa862f95c7829fd55ed4d2f9e11e9
lazzzykim <33sunghyeon@gmail.com>
2024-03-05 22:46:58 +0900

    🔨Refactor: Post - comment 양방향 관계 설정
    CommentResponse: comment toResponse 메서드 추가
    
    resolves: #1

--- src/main/kotlin/com/teamsparta/buysell/domain/comment/dto/response/CommentResponse.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/comment/dto/response/CommentResponse.kt
@@ -1,5 +1,20 @@
 package com.teamsparta.buysell.domain.comment.dto.response
 
+import com.teamsparta.buysell.domain.comment.model.Comment
+
 data class CommentResponse(
-    val message: String
-)
+    //댓글 내용
+    val content: String,
+    val createdName: String?
+){
+    companion object{
+        fun toResponse(
+            comment: Comment
+        ):CommentResponse{
+            return CommentResponse(
+                content = comment.content,
+                createdName = comment.createdName
+            )
+        }
+    }
+}
