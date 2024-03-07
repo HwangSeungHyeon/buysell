08887de3ed8ccfd2df773ea8800fd4e352ecfa34
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-07 15:49:50 +0900

    🔨Refactor : Like entity 수정
    사용하지 않는 코드 삭제
    
    Ref: #5

--- src/main/kotlin/com/teamsparta/buysell/domain/post/model/Like.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/post/model/Like.kt
@@ -4,20 +4,16 @@ import com.teamsparta.buysell.domain.exception.ForbiddenException
 import com.teamsparta.buysell.domain.member.model.Member
 import com.teamsparta.buysell.infra.security.UserPrincipal
 import jakarta.persistence.*
-import org.hibernate.annotations.OnDelete
-import org.hibernate.annotations.OnDeleteAction
 
 @Table(name = "post_likes")
 @Entity
 class Like private constructor(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
     var post: Post,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
     var member: Member,
 ) {
     @Id
