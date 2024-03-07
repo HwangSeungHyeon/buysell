010096769202e9735389670628505f62d4b4bf16
lazzzykim <33sunghyeon@gmail.com>
2024-03-07 20:32:31 +0900

    🔨Refactor: Account 주석 제거
    resolves: #8

--- src/main/kotlin/com/teamsparta/buysell/domain/member/model/Account.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/member/model/Account.kt
@@ -1,12 +1,6 @@
 package com.teamsparta.buysell.domain.member.model
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "account")
@@ -15,8 +9,6 @@ class Account (
     @Column(name = "account_balance")
     var accountBalance: Int = 0
 
-//    @OneToOne(mappedBy = "member")
-//    val member: Member
 ){
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +16,17 @@ class Account (
 
     fun modifyBalance(money: Int){
         accountBalance += money
+    }
+
+    fun payment(price: Int){
+        if(availableForPurchase(price)){ //결제 가능할 때
+            accountBalance -= price
+        } else{ //결제 불가능할 때 에러를 던진다
+            throw IllegalArgumentException("잔액이 부족합니다.")
+        }
+    }
+
+    fun availableForPurchase(price: Int): Boolean{
+        return accountBalance > price
     }
 }
