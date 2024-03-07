ec096f8bb3ebff8545255a711f7eeaf5533a36b3
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-07 10:33:28 +0900

    ☔️Test : Comment 테스트 코드 작성
    테스트는 h2 데이터베이스 사용
    build.gradle.kts 변경
    Comment Entity 단위 테스트
    Comment Service 통합 테스트
    
    Ref: #11

---
+++ src/test/kotlin/com/teamsparta/buysell/domain/common/BaseRepositoryTest.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.buysell.domain.common
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class BaseRepositoryTest {
+}
