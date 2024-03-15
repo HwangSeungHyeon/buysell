a4274fc7d276ba8b9a8dc4247531ad46089fe412
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-15 16:53:57 +0900

    🔨Refactor: Member 수정
    프로필 수정 API Validation 추가
    게시글 조회시 반환형 변환 PostResponse -> PostListResponse
    프로필 API 회원만 접근할 수 있도록 수정
    
    Ref: #12

--- src/main/kotlin/com/teamsparta/buysell/domain/member/dto/request/MemberProfileUpdateRequest.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/member/dto/request/MemberProfileUpdateRequest.kt
@@ -1,7 +1,16 @@
 package com.teamsparta.buysell.domain.member.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Pattern
+
 data class MemberProfileUpdateRequest(
 /*    val email : String,*/
+    @field:Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{2,8}$",
+        message = "닉네임은 숫자, 영어 대소문자를 포함한 최소 2자 최대 8자 이어야 합니다.")
+    @Schema(description = "수정할 닉네임", example = "test1234")
     val nickname: String?,
+
+    @field:Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "생년월일은 'yyyy-mm-dd' 형식으로 입력해주세요.")
     val birthday: String?
 )
--- src/main/kotlin/com/teamsparta/buysell/domain/member/service/ProfileService.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/member/service/ProfileService.kt
@@ -2,12 +2,12 @@ package com.teamsparta.buysell.domain.member.service
 
 import com.teamsparta.buysell.domain.member.dto.request.MemberProfileUpdateRequest
 import com.teamsparta.buysell.domain.member.dto.response.MemberResponse
-import com.teamsparta.buysell.domain.post.dto.response.PostResponse
+import com.teamsparta.buysell.domain.post.dto.response.PostListResponse
 import com.teamsparta.buysell.infra.security.UserPrincipal
 
 interface ProfileService {
-    fun getAllPostByMemberId(memberId:Int) : List<PostResponse>?
-    fun getAllPostByLike(userPrincipal: UserPrincipal): List<PostResponse>?
+    fun getAllPostByMemberId(memberId:Int) : List<PostListResponse>?
+    fun getAllPostByLike(userPrincipal: UserPrincipal): List<PostListResponse>?
     fun getMyProfile(userPrincipal: UserPrincipal): MemberResponse?
     fun updateMyProfile(userPrincipal: UserPrincipal, request: MemberProfileUpdateRequest): MemberResponse
 }
