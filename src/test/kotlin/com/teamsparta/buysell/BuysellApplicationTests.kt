1169eb6e6f7a3c98709635f1f5a5ec36deac1c92
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-19 23:03:41 +0900

    🔨Refactor: CI workflow 수정
    테스트 스텝 추가
    
    Ref: #54

--- src/test/kotlin/com/teamsparta/buysell/BuysellApplicationTests.kt
+++ src/test/kotlin/com/teamsparta/buysell/BuysellApplicationTests.kt
@@ -3,7 +3,7 @@ package com.teamsparta.buysell
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [BuysellApplicationTests::class])
 class BuysellApplicationTests {
 
     @Test
