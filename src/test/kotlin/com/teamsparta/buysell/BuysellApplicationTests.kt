67166a7367852040cb0d4b16fcc396f5102ae38d
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-20 04:35:50 +0900

    🔨Refactor: BuysellApplicationTests 수정
    Class 명을 지정하도록 변경

--- src/test/kotlin/com/teamsparta/buysell/BuysellApplicationTests.kt
+++ src/test/kotlin/com/teamsparta/buysell/BuysellApplicationTests.kt
@@ -3,6 +3,7 @@ package com.teamsparta.buysell
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+//class 지정해야 에러가 발생 X
 @SpringBootTest(classes = [BuysellApplicationTests::class])
 class BuysellApplicationTests {
 
