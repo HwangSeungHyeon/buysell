9a0655fd0ab9d2c6ffaaccb627cc164ff573947c
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-19 22:25:34 +0900

    ☔️Test: 댓글 Test 코드 작성
    CommentService 단위 테스트 수정
    Comment Entity 단위 테스트 수정
    
    Ref: #54

--- src/test/kotlin/com/teamsparta/buysell/domain/comment/model/CommentTest.kt
+++ src/test/kotlin/com/teamsparta/buysell/domain/comment/model/CommentTest.kt
@@ -1,127 +1,113 @@
-//package com.teamsparta.buysell.domain.comment.model
-//
-//import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
-//import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
-//import com.teamsparta.buysell.domain.exception.ForbiddenException
-//import com.teamsparta.buysell.domain.member.model.Member
-//import com.teamsparta.buysell.domain.member.model.Platform
-//import com.teamsparta.buysell.domain.member.model.Role
-//import com.teamsparta.buysell.domain.post.model.Category
-//import com.teamsparta.buysell.domain.post.model.Post
-//import com.teamsparta.buysell.infra.security.UserPrincipal
-//import io.kotest.assertions.throwables.shouldThrow
-//import io.kotest.matchers.shouldBe
-//import io.kotest.matchers.shouldNotBe
-//import org.junit.jupiter.api.Test
-//
-//class CommentTest {
-//
-//    // 판매자 객체
-//    private val member1 = Member(
-//        email = "test@gmail.com",
-//        password = "!test123",
-//        nickname = "데네브",
-//        role = Role.MEMBER,
-//        gender = "남자",
-//        birthday = "2024-03-06",
-//        platform = Platform.LOCAL,
-//    )
-//
-//    private val post = Post(
-//        title = "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이",
-//        content = "풀박스이고 25년 1월 4일까지 보증기간입니다\n" +
-//                "기스나 하자 없습니다\n" +
-//                "배터리 성능 100\n" +
-//                "인강용으로 사용했고 정말 깨끗하게 사용하였습니다\n" +
-//                "직거래는 삼송역 가능합니다\n" +
-//                "에눌 문의 정중히 사양합니다",
-//        view = 0,
-//        price = 950000,
-//        member = member1,
-//        category = Category.FOOD
-//    )
-//
-//    // 구매자 객체
-//    private val member2 = Member(
-//        email = "test2@gmail.com",
-//        password = "!test1234",
-//        nickname = "알타이르",
-//        role = Role.MEMBER,
-//        gender = "남자",
-//        birthday = "2024-03-06",
-//        platform = Platform.LOCAL,
-//    )
-//
-//    // 1. createRequest DTO가 Comment.makeEntity 메서드의 인자로 전달되었을 때 Comment 객체가 제대로 생성되는지 확인
-//    // 2. updateRequest DTO가 edit 메서드의 인자로 전달되었을 때 Comment 객체 내용이 제대로 수정되는지 확인
-//    // 3. 로그인한 사람과 댓글을 작성한 사람이 같은 사람일 경우
-//    // 4. 로그인한 사람과 댓글을 작성한 사람이 다른 사람일 경우
-//
-//    @Test
-//    fun `createRequest DTO 가 전달되었을 때 Comment 객체가 제대로 생성되는지 확인`(){
-//        // GIVEN : 테스트에 필요한 사전정보 생성
-//        val tempText = "테스트용으로 사용할 댓글"
-//        val createRequest = CreateRequest(tempText)
-//
-//        // WHEN : 테스트 할 메서드 호출
-//        val commentEntity = Comment.makeEntity(createRequest, member2, post)
-//
-//        // THEN : 결과값 검증
-//        commentEntity.id shouldBe null
-//        commentEntity.member.nickname shouldBe "알타이르"
-//        commentEntity.content shouldBe tempText
-//        commentEntity.post.title shouldBe "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이"
-//    }
-//
-//
-//    @Test
-//    fun `updateRequest DTO가 edit 메서드의 인자로 전달되었을 때 Comment 객체 내용이 제대로 수정되는지 확인`(){
-//        // GIVEN : 테스트에 필요한 사전정보 생성
-//        val tempText = "테스트용으로 사용할 댓글"
-//        val createRequest = CreateRequest(tempText)
-//        val commentEntity = Comment.makeEntity(createRequest, member2, post)
-//
-//        val editedText = "테스트용으로 사용할 댓글인데 수정도 했어"
-//        val updateRequest = UpdateRequest(editedText)
-//
-//        // WHEN : 테스트 할 메서드 호출
-//        commentEntity.edit(updateRequest)
-//
-//        // THEN : 결과값 검증
-//        commentEntity.content shouldBe editedText
-//    }
-//
-//    @Test
-//    fun `로그인한 사람과 댓글을 작성한 사람이 같은 사람일 경우`(){
-//        // GIVEN : 테스트에 필요한 사전정보 생성
-//        val tempText = "테스트용으로 사용할 댓글"
-//        val createRequest = CreateRequest(tempText)
-//        val commentEntity = Comment.makeEntity(createRequest, member2, post)
-//
-//        val userPrincipal = UserPrincipal(id = 1, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//        commentEntity.member.id = 1
-//
-//        // WHEN & THEN: 테스트 할 메서드 호출하고 결과값 검증
-//        commentEntity.checkPermission(userPrincipal)
-//    }
-//
-//    @Test
-//    fun `로그인한 사람과 댓글을 작성한 사람이 다른 사람일 경우 에러를 발생시키는지 확인`(){
-//        // GIVEN : 테스트에 필요한 사전정보 생성
-//        val tempText = "테스트용으로 사용할 댓글"
-//        val createRequest = CreateRequest(tempText)
-//        val commentEntity = Comment.makeEntity(createRequest, member2, post)
-//
-//        commentEntity.member.id = 1 //DB에 들어가서 id가 1이라고 가정
-//
-//        val userPrincipal = UserPrincipal(id = 100000, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//        // WHEN & THEN: 테스트 할 메서드 호출하고 결과값 검증
-//        val exception = shouldThrow<ForbiddenException> {
-//            commentEntity.checkPermission(userPrincipal)
-//        }
-//
-//        exception.message shouldBe "수정 권한이 없습니다."
-//    }
-//}
+package com.teamsparta.buysell.domain.comment.model
+
+import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
+import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
+import com.teamsparta.buysell.domain.exception.ForbiddenException
+import com.teamsparta.buysell.domain.member.model.Account
+import com.teamsparta.buysell.domain.member.model.Member
+import com.teamsparta.buysell.domain.member.model.Platform
+import com.teamsparta.buysell.domain.member.model.Role
+import com.teamsparta.buysell.domain.post.model.Category
+import com.teamsparta.buysell.domain.post.model.Post
+import com.teamsparta.buysell.infra.security.UserPrincipal
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class CommentTest: BehaviorSpec({
+    extension(SpringExtension)
+
+    val postWriter = Member(
+        email = "test@gmail.com",
+        password = "!test123",
+        nickname = "데네브",
+        role = Role.MEMBER,
+        gender = "남자",
+        birthday = "2024-03-06",
+        platform = Platform.LOCAL,
+        account = Account()
+    ).apply { id = 1 }
+
+    val ipadPost = Post(
+        title = "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이",
+        content = "풀박스이고 25년 1월 4일까지 보증기간입니다\n" +
+                "기스나 하자 없습니다\n" +
+                "배터리 성능 100\n" +
+                "인강용으로 사용했고 정말 깨끗하게 사용하였습니다\n" +
+                "직거래는 삼송역 가능합니다\n" +
+                "에눌 문의 정중히 사양합니다",
+        view = 0,
+        price = 950000,
+        member = postWriter,
+        category = Category.FOOD
+    ).apply { id = 1 }
+
+    val consumer = Member(
+        email = "test2@gmail.com",
+        password = "!test123",
+        nickname = "알타이르",
+        role = Role.MEMBER,
+        gender = "남자",
+        birthday = "2024-03-06",
+        platform = Platform.LOCAL,
+        account = Account(),
+        order = setOf()
+    ).apply { id = 2 }
+
+    val createRequest = CreateRequest("댓글 테스트")
+
+    given("Comment 객체가 없을 경우") {
+
+        `when`("createRequest DTO가 Comment.makeEntity 메서드의 인자로 전달되었다면") {
+            then("객체가 생성되어야 한다.") {
+                val commentEntity = Comment.makeEntity(createRequest, consumer, ipadPost)
+
+                commentEntity.id shouldBe null
+                commentEntity.member.nickname shouldBe "알타이르"
+                commentEntity.content shouldBe "댓글 테스트"
+                commentEntity.post.title shouldBe "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이"
+            }
+        }
+    }
+
+    given("Comment 객체가 있을 경우") {
+        val commentEntity = Comment.makeEntity(createRequest, consumer, ipadPost)
+
+        `when`("updateRequest DTO가 edit 메서드의 인자로 전달되었다면") {
+            val updateRequest = UpdateRequest("수정된 댓글 테스트")
+            then("객체가 수정되어야 한다.") {
+                commentEntity.edit(updateRequest)
+
+                commentEntity.id shouldBe null
+                commentEntity.member.nickname shouldBe "알타이르"
+                commentEntity.content shouldBe "수정된 댓글 테스트"
+                commentEntity.post.title shouldBe "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이"
+            }
+        }
+
+        `when`("로그인한 사람과 댓글을 작성한 사람이 같은 사람일 경우") {
+            val principal = UserPrincipal(id = 2, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+
+            then("아무런 값도 반환하지 않는다.") {
+                commentEntity.checkPermission(principal)
+
+                commentEntity.member.id shouldBe 2
+            }
+        }
+
+        `when`("로그인한 사람과 댓글을 작성한 사람이 다른 사람일 경우") {
+            val principal = UserPrincipal(id = 9999, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+            then("ForbiddenException 에러를 발생시킨다.") {
+                val exception = shouldThrow<ForbiddenException> {
+                    commentEntity.checkPermission(principal)
+                }
+
+                exception.message shouldBe "수정 권한이 없습니다."
+            }
+        }
+    }
+})
--- src/test/kotlin/com/teamsparta/buysell/domain/comment/service/CommentServiceUnitTest.kt
+++ src/test/kotlin/com/teamsparta/buysell/domain/comment/service/CommentServiceUnitTest.kt
@@ -1,192 +1,191 @@
-//package com.teamsparta.buysell.domain.comment.service
-//
-//import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
-//import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
-//import com.teamsparta.buysell.domain.comment.model.Comment
-//import com.teamsparta.buysell.domain.comment.repository.CommentRepository
-//import com.teamsparta.buysell.domain.exception.ForbiddenException
-//import com.teamsparta.buysell.domain.exception.ModelNotFoundException
-//import com.teamsparta.buysell.domain.member.model.Account
-//import com.teamsparta.buysell.domain.member.model.Member
-//import com.teamsparta.buysell.domain.member.model.Platform
-//import com.teamsparta.buysell.domain.member.model.Role
-//import com.teamsparta.buysell.domain.member.repository.MemberRepository
-//import com.teamsparta.buysell.domain.post.model.Category
-//import com.teamsparta.buysell.domain.post.model.Post
-//import com.teamsparta.buysell.domain.post.repository.PostRepository
-//import com.teamsparta.buysell.infra.security.UserPrincipal
-//import io.kotest.assertions.throwables.shouldThrow
-//import io.kotest.core.spec.style.BehaviorSpec
-//import io.kotest.extensions.spring.SpringExtension
-//import io.kotest.matchers.shouldBe
-//import io.mockk.clearAllMocks
-//import io.mockk.every
-//import io.mockk.junit5.MockKExtension
-//import io.mockk.mockk
-//import io.mockk.verify
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.springframework.data.repository.findByIdOrNull
-//
-//@ExtendWith(MockKExtension::class)
-//class CommentServiceUnitTest: BehaviorSpec({
-//    extension(SpringExtension)
-//
-//    afterContainer {
-//        clearAllMocks()
-//    }
-//
-//    // 종속 repository mocking
-//    val commentRepository = mockk<CommentRepository>()
-//    val postRepository = mockk<PostRepository>()
-//    val memberRepository = mockk<MemberRepository>()
-//
-//    // courseService 생성
-//    val commentService = CommentServiceImpl(commentRepository, postRepository, memberRepository)
-//
-//    val postId = 1
-//    val postWriterId = 1
-//    val consumerId = 2
-//    val commentId = 1
-//
-//    val postWriter = Member(
-//        email = "test@gmail.com",
-//        password = "!test123",
-//        nickname = "데네브",
-//        role = Role.MEMBER,
-//        gender = "남자",
-//        birthday = "2024-03-06",
-//        platform = Platform.LOCAL,
-//        account = Account()
-//    ).apply { id = postWriterId }
-//
-//    val ipadPost = Post(
-//        title = "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이",
-//        content = "풀박스이고 25년 1월 4일까지 보증기간입니다\n" +
-//                "기스나 하자 없습니다\n" +
-//                "배터리 성능 100\n" +
-//                "인강용으로 사용했고 정말 깨끗하게 사용하였습니다\n" +
-//                "직거래는 삼송역 가능합니다\n" +
-//                "에눌 문의 정중히 사양합니다",
-//        view = 0,
-//        price = 950000,
-//        member = postWriter,
-//        category = Category.FOOD
-//    ).apply { id = postId }
-//
-//    val consumer = Member(
-//        email = "test2@gmail.com",
-//        password = "!test123",
-//        nickname = "데네브",
-//        role = Role.MEMBER,
-//        gender = "남자",
-//        birthday = "2024-03-06",
-//        platform = Platform.LOCAL,
-//        account = Account(),
-//        order = setOf()
-//    ).apply { id = consumerId }
-//
-//    val createRequest = CreateRequest("테스트용으로 사용할 댓글")
-//    val updateRequest = UpdateRequest("테스트용으로 사용할 댓글인데 수정도 했어")
-//    val comment = Comment.makeEntity(request = createRequest, member = consumer, post = ipadPost).apply { id = commentId }
-//
-//    given("댓글을 작성하려고 할 때"){
-//        val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//        `when`("댓글을 달 게시글이 없거나 댓글 작성자 정보가 DB에 없으면"){
-//            then("ModelNotFound 에러가 발생해야 한다."){
-//                every { postRepository.findByIdOrNull(any()) } returns null
-//                every { memberRepository.findByIdOrNull(any()) } returns null
-//
-//                shouldThrow<ModelNotFoundException> {
-//                    commentService.addComment(postId = postId, request = createRequest, principal = principal)
-//                }
-//            }
-//        }
-//        `when`("댓글을 달 게시글이 있고,댓 글 작성자 정보도 DB에 있으면"){
-//            then("댓글이 저장되어야 한다."){
-//                every { postRepository.findByIdOrNull(any()) } returns ipadPost
-//                every { memberRepository.findByIdOrNull(any()) } returns consumer
-//                every { commentRepository.save(any()) } returns comment
-//
-//                val result = commentService.addComment(postId = postId, request = createRequest, principal = principal)
-//
-//                result.message shouldBe "댓글이 작성되었습니다."
-//            }
-//        }
-//    }
-//
-//    given("댓글을 수정하려고 할 때"){
-//        `when`("수정할 댓글이 없으면"){
-//            then("ModelNotFound 에러가 발생해야 한다."){
-//                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns null
-//
-//                shouldThrow<ModelNotFoundException> {
-//                    commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
-//                }
-//            }
-//        }
-//
-//        `when`("수정할 권한이 없으면"){
-//            then("ForbiddenException 에러가 발생해야 한다."){
-//                val principal = UserPrincipal(id = 5, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
-//
-//                val exception = shouldThrow<ForbiddenException> {
-//                    commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
-//                }
-//                exception.message shouldBe "수정 권한이 없습니다."
-//            }
-//        }
-//
-//        `when`("수정할 권한이 있으면"){
-//            then("댓글이 수정되어야 한다."){
-//                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
-//
-//                val result = commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
-//                result.message shouldBe "댓글이 수정되었습니다."
-//            }
-//        }
-//    }
-//
-//    given("댓글을 삭제하려고 할 때"){
-//        `when`("삭제할 댓글이 없으면"){
-//            then("ModelNotFound 에러가 발생해야 한다."){
-//                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns null
-//
-//                shouldThrow<ModelNotFoundException> {
-//                    commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
-//                }
-//            }
-//        }
-//
-//        `when`("삭제할 권한이 없으면"){
-//            then("ForbiddenException 에러가 발생해야 한다."){
-//                val principal = UserPrincipal(id = 5, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
-//
-//                val exception = shouldThrow<ForbiddenException> {
-//                    commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
-//                }
-//                exception.message shouldBe "수정 권한이 없습니다."
-//            }
-//        }
-//
-//        `when`("삭제할 권한이 있으면"){
-//            then("댓글이 삭제되어야 한다."){
-//                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
-//
-//                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
-//                every { commentRepository.delete(any()) }
-//
-//                val result = commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
-//                result.message shouldBe "댓글이 삭제되었습니다."
-//            }
-//        }
-//    }
-//
-//})
+package com.teamsparta.buysell.domain.comment.service
+
+import com.teamsparta.buysell.domain.comment.dto.request.CreateRequest
+import com.teamsparta.buysell.domain.comment.dto.request.UpdateRequest
+import com.teamsparta.buysell.domain.comment.model.Comment
+import com.teamsparta.buysell.domain.comment.repository.CommentRepository
+import com.teamsparta.buysell.domain.exception.ForbiddenException
+import com.teamsparta.buysell.domain.exception.ModelNotFoundException
+import com.teamsparta.buysell.domain.member.model.Account
+import com.teamsparta.buysell.domain.member.model.Member
+import com.teamsparta.buysell.domain.member.model.Platform
+import com.teamsparta.buysell.domain.member.model.Role
+import com.teamsparta.buysell.domain.member.repository.MemberRepository
+import com.teamsparta.buysell.domain.post.model.Category
+import com.teamsparta.buysell.domain.post.model.Post
+import com.teamsparta.buysell.domain.post.repository.PostRepository
+import com.teamsparta.buysell.infra.security.UserPrincipal
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.data.repository.findByIdOrNull
+
+@ExtendWith(MockKExtension::class)
+class CommentServiceUnitTest: BehaviorSpec({
+    extension(SpringExtension)
+
+    afterContainer {
+        clearAllMocks()
+    }
+
+    // 종속 repository mocking
+    val commentRepository = mockk<CommentRepository>()
+    val postRepository = mockk<PostRepository>()
+    val memberRepository = mockk<MemberRepository>()
+
+    // courseService 생성
+    val commentService = CommentServiceImpl(commentRepository, postRepository, memberRepository)
+
+    val postId = 1
+    val postWriterId = 1
+    val consumerId = 2
+    val commentId = 1
+
+    val postWriter = Member(
+        email = "test@gmail.com",
+        password = "!test123",
+        nickname = "데네브",
+        role = Role.MEMBER,
+        gender = "남자",
+        birthday = "2024-03-06",
+        platform = Platform.LOCAL,
+        account = Account()
+    ).apply { id = postWriterId }
+
+    val ipadPost = Post(
+        title = "아이패드 프로 4세대 m2 11인치 128기가 스페이스그레이",
+        content = "풀박스이고 25년 1월 4일까지 보증기간입니다\n" +
+                "기스나 하자 없습니다\n" +
+                "배터리 성능 100\n" +
+                "인강용으로 사용했고 정말 깨끗하게 사용하였습니다\n" +
+                "직거래는 삼송역 가능합니다\n" +
+                "에눌 문의 정중히 사양합니다",
+        view = 0,
+        price = 950000,
+        member = postWriter,
+        category = Category.FOOD
+    ).apply { id = postId }
+
+    val consumer = Member(
+        email = "test2@gmail.com",
+        password = "!test123",
+        nickname = "알타이르",
+        role = Role.MEMBER,
+        gender = "남자",
+        birthday = "2024-03-06",
+        platform = Platform.LOCAL,
+        account = Account(),
+        order = setOf()
+    ).apply { id = consumerId }
+
+    val createRequest = CreateRequest("테스트용으로 사용할 댓글")
+    val updateRequest = UpdateRequest("테스트용으로 사용할 댓글인데 수정도 했어")
+    val comment = Comment.makeEntity(request = createRequest, member = consumer, post = ipadPost).apply { id = commentId }
+
+    given("댓글을 작성하려고 할 때"){
+        val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+        `when`("댓글을 달 게시글이 없거나 댓글 작성자 정보가 DB에 없으면"){
+            then("ModelNotFound 에러가 발생해야 한다."){
+                every { postRepository.findByIdOrNull(any()) } returns null
+                every { memberRepository.findByIdOrNull(any()) } returns null
+
+                shouldThrow<ModelNotFoundException> {
+                    commentService.addComment(postId = postId, request = createRequest, principal = principal)
+                }
+            }
+        }
+        `when`("댓글을 달 게시글이 있고,댓 글 작성자 정보도 DB에 있으면"){
+            then("댓글이 저장되어야 한다."){
+                every { postRepository.findByIdOrNull(any()) } returns ipadPost
+                every { memberRepository.findByIdOrNull(any()) } returns consumer
+                every { commentRepository.save(any()) } returns comment
+
+                val result = commentService.addComment(postId = postId, request = createRequest, principal = principal)
+
+                result.message shouldBe "댓글이 작성되었습니다."
+            }
+        }
+    }
+
+    given("댓글을 수정하려고 할 때"){
+        `when`("수정할 댓글이 없으면"){
+            then("ModelNotFound 에러가 발생해야 한다."){
+                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns null
+
+                shouldThrow<ModelNotFoundException> {
+                    commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
+                }
+            }
+        }
+
+        `when`("수정할 권한이 없으면"){
+            then("ForbiddenException 에러가 발생해야 한다."){
+                val principal = UserPrincipal(id = 5, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
+
+                val exception = shouldThrow<ForbiddenException> {
+                    commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
+                }
+                exception.message shouldBe "수정 권한이 없습니다."
+            }
+        }
+
+        `when`("수정할 권한이 있으면"){
+            then("댓글이 수정되어야 한다."){
+                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
+
+                val result = commentService.editComment(postId = postId, commentId = commentId, request = updateRequest, principal = principal)
+                result.message shouldBe "댓글이 수정되었습니다."
+            }
+        }
+    }
+
+    given("댓글을 삭제하려고 할 때"){
+        `when`("삭제할 댓글이 없으면"){
+            then("ModelNotFound 에러가 발생해야 한다."){
+                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns null
+
+                shouldThrow<ModelNotFoundException> {
+                    commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
+                }
+            }
+        }
+
+        `when`("삭제할 권한이 없으면"){
+            then("ForbiddenException 에러가 발생해야 한다."){
+                val principal = UserPrincipal(id = 5, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
+
+                val exception = shouldThrow<ForbiddenException> {
+                    commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
+                }
+                exception.message shouldBe "수정 권한이 없습니다."
+            }
+        }
+
+        `when`("삭제할 권한이 있으면"){
+            then("댓글이 삭제되어야 한다."){
+                val principal = UserPrincipal(id = consumerId, email = "test2@gmail.com", role = setOf("MEMBER"), platform = "LOCAL")
+
+                every { commentRepository.findByPostIdAndId(any(), any()) } returns comment
+                every { commentRepository.delete(any()) } returns Unit
+
+                val result = commentService.deleteComment(postId = postId, commentId = commentId, principal = principal)
+                result.message shouldBe "댓글이 삭제되었습니다."
+            }
+        }
+    }
+
+})
