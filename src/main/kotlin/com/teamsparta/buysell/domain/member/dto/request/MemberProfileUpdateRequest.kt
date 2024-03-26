18a8a54edd0097cdbfed8c8c0136190523e794d1
HwangSeungHyeon <xlddy02@gmail.com>
2024-03-26 15:16:49 +0900

    🔨Refactor: 멤버 프로필 수정
    닉네임 수정시, 사용중인 닉네임이 있는지 검사하는 로직 추가
    
    # Ref: #12

--- src/main/kotlin/com/teamsparta/buysell/domain/member/dto/request/MemberProfileUpdateRequest.kt
+++ src/main/kotlin/com/teamsparta/buysell/domain/member/dto/request/MemberProfileUpdateRequest.kt
@@ -9,8 +9,8 @@ data class MemberProfileUpdateRequest(
         regexp = "^[\\w가-힣]{2,8}$",
         message = "닉네임은 숫자, 영어 대소문자, 한글을 포함한 최소 2자 최대 8자 이어야 하며, 중복, 공백은 허용하지 않습니다.")
     @Schema(description = "회원가입에 사용할 닉네임", example = "test123")
-    val nickname: String?,
+    val nickname: String,
 
     @field:Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "생년월일은 'yyyy-mm-dd' 형식으로 입력해주세요.")
-    val birthday: String?
+    val birthday: String
 )
