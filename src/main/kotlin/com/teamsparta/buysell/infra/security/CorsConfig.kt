26d622de9957b3946fc1d102df357e311eea93d7
codekmj1 <codekmj@gmail.com>
2024-04-03 21:26:12 +0900

    🔨Refactor: Cors 주석 해제

--- src/main/kotlin/com/teamsparta/buysell/infra/security/CorsConfig.kt
+++ src/main/kotlin/com/teamsparta/buysell/infra/security/CorsConfig.kt
@@ -1,24 +1,24 @@
-//package com.teamsparta.buysell.infra.security
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.web.cors.CorsConfiguration
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-//import org.springframework.web.filter.CorsFilter
-//
-//@Configuration
-//class CorsConfig {
-//    @Bean
-//    fun corsFilter(): CorsFilter {
-//        val source = UrlBasedCorsConfigurationSource()
-//        val config = CorsConfiguration().apply {
-//            allowCredentials = true
-//            addAllowedOriginPattern("*")
-//            addAllowedHeader("*")
-//            addAllowedMethod("*") // 요청 허용 메서드
-//            addExposedHeader("authorization") // 클라이언트에 노출할 헤더 추가
-//        }
-//        source.registerCorsConfiguration("/**", config)
-//        return CorsFilter(source)
-//    }
-//}
+package com.teamsparta.buysell.infra.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration().apply {
+            allowCredentials = true
+            addAllowedOriginPattern("*")
+            addAllowedHeader("*")
+            addAllowedMethod("*") // 요청 허용 메서드
+            addExposedHeader("authorization") // 클라이언트에 노출할 헤더 추가
+        }
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
